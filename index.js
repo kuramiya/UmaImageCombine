a93a1d4aa0700537a34d85f5cbeb8ff979119ffa
seki <hiroyuki.seki@horiba.com>
2023-03-16 17:18:58 +0900

    洗濯処理、マウス処理のイベントを追加した

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const canvasSize_px = 600;
+const maxRowCount = 4;
+const maxColumnCount = 4;
+
+let currentRowCount = 2;
+let currentColumnCount = 2;
 
 //  画像配置用の連想配列
-//  row1col1といった名前で保存する
+//  row0col0といった名前で保存する
+//  まずはnull画像で初期化しておく
 let imageArr = {};
-imageArr["row0col0"] = "/image/null2.png";
-imageArr["row0col1"] = "/image/img1.png";
-imageArr["row1col0"] = "/image/img2.png";
-imageArr["row1col1"] = "/image/img3.png";
+for (let rowIndex = 0; rowIndex < maxRowCount; rowIndex++)
+{
+    for (let columnIndex = 0; columnIndex < maxColumnCount; columnIndex++)
+    {
+        imageArr["row" + rowIndex + "col" + columnIndex] = "/image/null2.png";        
+    }
+}
 
 //  配置画像を表示する処理
 function showImages(ctx, rowCount, colCount)
@@ -38,6 +47,30 @@ function showImages(ctx, rowCount, colCount)
     }
 }
 
+//  行コンボボックスが変更された時の処理
+function rowCountChange(e)
+{
+    currentRowCount = parseInt(e.target.value)
+
+    console.log("row count:" + currentRowCount);
+}
+
+function columnCountChange(e)
+{
+    currentColumnCount = parseInt(e.target.value)
+
+    console.log("column count:" + currentColumnCount);
+}
+
+function canvasClick(e)
+{
+    let rect = e.target.getBoundingClientRect();
+    let clickPosX = e.clientX - rect.left;
+    let clickPosY = e.clientY - rect.top;
+
+    console.log("x pos:" + clickPosX + " y pos:" + clickPosY);
+}
+
 window.onload = () =>
 {
     // canvas準備
@@ -46,17 +79,9 @@ window.onload = () =>
 
     showImages(ctx, 2, 2);
 
-    // // 画像読み込み
-    // const chara = new Image();
-    // chara.src = "/image/img1.png";  // 画像のURLを指定
-    // chara.onload = () => {
-    //     ctx.drawImage(chara, 0, 0, 200, 200);
-    // };
-
-    // const chara2 = new Image();
-    // chara2.src = "/image/null.png";  // 画像のURLを指定
-    // chara2.onload = () => {
-    //     ctx.drawImage(chara2, 200, 200, 200, 200);
-    // };
 };
 
+//  イベント登録
+document.querySelector("#rowCountSelect").addEventListener("change", rowCountChange, false); 
+document.querySelector("#columnCountSelect").addEventListener("change", columnCountChange, false); 
+document.querySelector("#combinedImage").addEventListener("click", canvasClick, false); 
