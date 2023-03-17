206a4ab416c6b430a317f82bad744c3a610fd1aa
seki <hiroyuki.seki@horiba.com>
2023-03-17 17:11:09 +0900

    bootstrapをダウンロード、モーダルダイアログを実装検討中

--- README.md
+++ README.md
@@ -9,6 +9,30 @@
 
 https://getbootstrap.jp/
 
+### ポップアップ画面の処理
+
+Modalを利用してみる
+
+https://getbootstrap.jp/docs/5.0/components/modal/
+
+どのようにして、canvasのクリックでモーダルを呼び出すか？  
+Javascriptで呼び出す方法が記載されていた  
+しかし、importがうまくいかない  
+npmでパッケージを入れないと駄目っぽい
+nodejsをインストールする  
+
+https://nodejs.org/ja
+
+nodejsのコマンドプロンプトで当該のフォルダに行き、npmを実行  
+プロキシを通す必要があった
+
+https://qiita.com/tenten0213/items/7ca15ce8b54acc3b5719
+
+それでも駄目。  
+そもそもこういう類はモジュールとして使用できないらしい。
+別の実装を検討する必要あり……。
+
+
 ## merge-images (不採用)
 
 画像連結用  
@@ -17,7 +41,7 @@ https://getbootstrap.jp/
 
 https://www.npmjs.com/package/merge-images
 
-## 設計（雑）
+# 設計（雑）
 
 - 行、列を設定可能にする
   - 最大4x4
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import * as bootstrap from "./bootstrap-5.0.2-dist/js/bootstrap.bundle.js";
 
 //  最大行数
 const maxRowCount = 4;
@@ -64,14 +65,13 @@ function canvasSizeChange(e)
 {
     currentCanvasSize = parseInt(e.target.value);
 
-    let canvas = document.querySelector("#combinedImage");
+    let canvas = document.querySelector("#outputCanvas");
 
     canvas.width = currentCanvasSize;
     canvas.height = currentCanvasSize;
 
     updateCanvas();
 }
-
 
 //  数コンボボックスが変更された時の処理
 function rowCountChange(e)
@@ -97,12 +97,17 @@ function canvasClick(e)
     let clickPosY = e.clientY - rect.top;
 
     console.log("x pos:" + clickPosX + " y pos:" + clickPosY);
+
+    //  これはダメ、モジュールとしてロードできない
+    var myModal = new bootstrap.Modal(document.getElementById('exampleModal'));
+    
+    myModal.show();
 }
 
 //  画像を再描画する処理
 function updateCanvas()
 {
-    const board = document.querySelector("#combinedImage");
+    const board = document.querySelector("#outputCanvas");
     const ctx = board.getContext("2d");
 
     showImages(ctx);
@@ -112,7 +117,7 @@ function updateCanvas()
 document.querySelector("#canvasSizeSelect").addEventListener("change", canvasSizeChange, false); 
 document.querySelector("#rowCountSelect").addEventListener("change", rowCountChange, false); 
 document.querySelector("#columnCountSelect").addEventListener("change", columnCountChange, false); 
-document.querySelector("#combinedImage").addEventListener("click", canvasClick, false);
+document.querySelector("#outputCanvas").addEventListener("click", canvasClick, false);
 
 window.onload = () =>
 {
